712de3fd99b608b910b327d1980e8fcd58bf790d
Anatiel Santos <anatielsantos@gmail.com>
2020-10-04 16:23:44 -0300

    Editing Tensorflow test file.

--- util/teste_tensorflow.py
+++ util/teste_tensorflow.py
@@ -1,6 +1,3 @@
-import os
-os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"   # see issue #152
-os.environ["CUDA_VISIBLE_DEVICES"]="0"
+import tensorflow as tf
 
-from tensorflow.python.client import device_lib
-print(device_lib.list_local_devices())
+print(tf.reduce_sum(tf.random.normal([1000, 1000])))
