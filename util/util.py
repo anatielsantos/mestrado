bdc0f55465b0cee76f0f0adcf5f965bdb8a88bca
Anatiel Santos <anatielsantos@gmail.com>
2021-02-08 23:22:53 -0300

    Ajustando funçao save slice para salvar imagens como array .npz.

--- util/util.py
+++ util/util.py
@@ -2,6 +2,7 @@ import SimpleITK as sitk
 import glob
 import numpy as np
 from skimage.color import rgb2gray
+from skimage.transform import resize
 from skimage.io import imsave
 import cv2
 
@@ -10,9 +11,9 @@ os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 conjunto = "lung_and_infection_mask"
-path_train = glob.glob(f"/home/anatielsantos/mestrado/bases/covid-19/{conjunto}/train/*.nii")
-path_test = glob.glob(f"/home/anatielsantos/mestrado/bases/covid-19/{conjunto}/test/*.nii")
-path_val = glob.glob(f"/home/anatielsantos/mestrado/bases/covid-19/{conjunto}/val/*.nii")
+path_train = glob.glob("/home/anatielsantos/mestrado/bases/covid-19-nii/infection_mask/train/*.nii")
+path_test = glob.glob("/home/anatielsantos/mestrado/bases/covid-19-nii/infection_mask/test/*.nii")
+path_val = glob.glob("/home/anatielsantos/mestrado/bases/covid-19-nii/infection_mask/val/*.nii")
 print("Train: ", len(path_train), "imagens")
 print("Test: ", len(path_test), "imagens")
 print("Val: ", len(path_val), "imagens")
@@ -76,26 +77,40 @@ def resize_image(images, path_save, conjunto):
 # group =  grupo (train, test, val)
 # contagem inicial da imagem dentro do grupo (nomes sequenciais nos 3 grupos)
 def save_slice(path_image, group, num_image = 0):
-            
+    images = None
     for i in range(len(path_image)):
-
         img = sitk.ReadImage(path_image[i])
         img_array = sitk.GetArrayFromImage(img)
-        new_slice = np.ones((img_array.shape[1], img_array.shape[2]))
+        resized_image_array = np.zeros((img_array.shape[0],256,256),dtype=np.float64)
+        print(img_array.shape,np.unique(img_array))
+        for slice_id in range(img_array.shape[0]):
+            resized_image_array[slice_id]=resize(img_array[slice_id],(256,256),preserve_range=True)
+        print(resized_image_array.shape,np.unique(resized_image_array))
+        if images is None:
+            images=resized_image_array
+        else:
+            images = np.concatenate([images,resized_image_array])
 
-        for p in range(img_array.shape[0]):
-            for l in range(img_array.shape[1]):
-                for c in range(img_array.shape[2]):
-                    new_slice[l, c] = img_array[p, l, c]
+        images = (images >0)*1
+    print(images.shape)
+    np.savez_compressed(f"/home/anatielsantos/workspace_visual/mestrado/datasets/covid19/B-lesion/{group}.npz",images)
+        # exit()
+        # new_slice = np.ones((img_array.shape[1], img_array.shape[2]))
 
-            print("Salvando imagem ", i, "slice ", p)
-            new_slice_gray = rgb2gray(new_slice)
+        # for p in range(img_array.shape[0]):
+        #     for l in range(img_array.shape[1]):
+        #         for c in range(img_array.shape[2]):
+        #             new_slice[l, c] = img_array[p, l, c]
 
-            imsave(f"/home/anatielsantos/workspace_visual/mestrado/datasets/covid19/B-lung-lesion/{group}/img{i+num_image}_slc{p}.jpg", new_slice_gray, check_contrast=False)
+        #     print("Salvando imagem ", i, "slice ", p)
+            
+        #     new_slice_gray = rgb2gray(new_slice)
 
-#save_slice(path_train, "train", 0)
-#save_slice(path_test, "test", 6)
-#save_slice(path_val, "val", 8)
+            #imsave(f"/home/anatielsantos/workspace_visual/mestrado/datasets/covid19/B-lung-lesion/{group}/img{i+num_image}_slc{p}.jpg", new_slice_gray, check_contrast=False)
+
+save_slice(path_train, "train", 0)
+save_slice(path_test, "test", 6)
+save_slice(path_val, "val", 8)
 
 ttv = "val"
 images = glob.glob(f"/home/anatielsantos/mestrado/bases/bases_dissertacao/datasets_covid19/datasets/covid19/A/{ttv}/*.jpg")
