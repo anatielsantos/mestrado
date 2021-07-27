01cabb91e01b2d5a33ea06f25b3a6a9e1171b990
Anatiel Santos <anatielsantos@gmail.com>
2021-07-27 19:29:50 -0300

    .

--- unet/data_covid.py
+++ unet/data_covid.py
@@ -32,8 +32,8 @@ def dice_bce_loss(y_true, y_pred):
 def load_train_data():
     imgs_train_npz = np.load('/data/flavio/anatiel/datasets/A/512x512/train_image.npz', allow_pickle=True)
     masks_train_npz = np.load('/data/flavio/anatiel/datasets/B_lung/512x512/train_mask.npz', allow_pickle=True)
-    imgs_train = imgs_train_npz['arr_0'][0]
-    masks_train = masks_train_npz['arr_0'][0]
+    imgs_train = imgs_train_npz['arr_0']
+    masks_train = masks_train_npz['arr_0']
     
     return imgs_train, masks_train
 
