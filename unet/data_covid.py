bb89e89bbfa2265de83cbd75afeab500f212878f
Anatiel Santos <anatielsantos@gmail.com>
2021-07-26 22:50:07 -0300

    Config Unet segment lungs.

--- unet/data_covid.py
+++ unet/data_covid.py
@@ -31,7 +31,7 @@ def dice_bce_loss(y_true, y_pred):
 
 def load_train_data():
     imgs_train_npz = np.load('/data/flavio/anatiel/datasets/A/512x512/ttv.npz')
-    masks_train_npz = np.load('/data/flavio/anatiel/datasets/B_lesion/512x512/ttv.npz')
+    masks_train_npz = np.load('/data/flavio/anatiel/datasets/B_lung/512x512/ttv.npz')
     imgs_train = imgs_train_npz['arr_0']
     masks_train = masks_train_npz['arr_0']
     
