d60ce20251108d6a165c34115e08691beeb8ace4
Anatiel Santos <anatielsantos@gmail.com>
2021-08-20 19:09:47 -0300

    .

--- dissertacao/lungseg/compress_dataset.py
+++ dissertacao/lungseg/compress_dataset.py
@@ -46,7 +46,7 @@ def load_image(path_image, path_mask, remove_no_lesion = False):
         # itkImage = sitk.GetImageFromArray(npyImage)
         # sitk.WriteImage(itkImage, '/home/anatielsantos/mestrado/datasets/dissertacao/train/image/train_images.nii.gz')
 
-        return [np.asarray(npyImage), np.asarray(npyMask)]
+        return [npyImage, npyMask]
 
     except Exception as e:
         print("type error: " + str(e))
@@ -94,7 +94,7 @@ def compress_dataset(src_dir, mask_dir, dst_dir, ext, joint, reverse = False, de
 def main():
     ext = '.nii.gz'
     joint = 'test' # [train, test]
-    main_dir_image = f'/home/anatielsantos/mestrado/datasets/dissertacao/{joint}/image/lung_extracted'
+    main_dir_image = f'/home/anatielsantos/mestrado/datasets/dissertacao/{joint}/image'
     main_dir_mask = f'/home/anatielsantos/mestrado/datasets/dissertacao/{joint}/mask'
     
     src = main_dir_image
