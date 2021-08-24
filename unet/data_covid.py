9f7affd6c9ad3ffd5455a054ecc2b38abaa5e987
Anatiel Santos <anatielsantos@gmail.com>
2021-08-23 23:51:49 -0300

    New training config for Unet.

--- unet/data_covid.py
+++ unet/data_covid.py
@@ -35,8 +35,7 @@ def load_train_data():
     imgs_train = imgs_train_npz['arr_0']
     masks_train = masks_train_npz['arr_0']
     
-    # return np.expand_dims(np.concatenate(imgs_train), axis=-1), np.expand_dims(np.concatenate(masks_train), axis=-1)
-    return imgs_train, masks_train
+    return np.expand_dims(np.concatenate(imgs_train), axis=-1), np.expand_dims(np.concatenate(masks_train), axis=-1)
 
 def load_val_data():
     imgs_val_npz = np.load('/data/flavio/anatiel/datasets/A/512x512/val_masked_lung.npz')
