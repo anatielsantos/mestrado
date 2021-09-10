57317ee142c2059a404f45f9c6422e69c28ef9a4
Anatiel Santos <anatielsantos@gmail.com>
2021-09-10 01:13:10 -0300

    New dataset1 configuration for lung segmentation training.

--- unet/lungseg/data_covid_lungseg.py
+++ unet/lungseg/data_covid_lungseg.py
@@ -30,8 +30,8 @@ def dice_bce_loss(y_true, y_pred):
     return dice_bce_loss
 
 def load_train_data():
-    imgs_train_npz = np.load('/data/flavio/anatiel/datasets/dissertacao/train_images_dataset1_lung_extract.npz', allow_pickle=True)
-    masks_train_npz = np.load('/data/flavio/anatiel/datasets/dissertacao/train_masks_dataset1_lung_extract.npz', allow_pickle=True)
+    imgs_train_npz = np.load('/data/flavio/anatiel/datasets/dissertacao/train_images_ds1_equalize_histt.npz', allow_pickle=True)
+    masks_train_npz = np.load('/data/flavio/anatiel/datasets/dissertacao/train_masks_ds1_equalize_hist.npz', allow_pickle=True)
     imgs_train = imgs_train_npz['arr_0']
     masks_train = masks_train_npz['arr_0']
     
