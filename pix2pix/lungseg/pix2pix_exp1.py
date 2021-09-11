221c0e74f0367f3cdf5d28e468faf5e56738d69c
Anatiel Santos <anatielsantos@gmail.com>
2021-09-10 22:02:24 -0300

    .

--- pix2pix/lungseg/pix2pix_exp1.py
+++ pix2pix/lungseg/pix2pix_exp1.py
@@ -1,7 +1,7 @@
 # GPU
 import os
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"] = "4"
+os.environ["CUDA_VISIBLE_DEVICES"] = "5"
 
 #configuração necessária nas GPU's RTX 
 import tensorflow as tf
