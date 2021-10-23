4ace77bfcfe667fc1ad197571ff6eef043de7ac5
Anatiel Santos <anatielsantos@gmail.com>
2021-10-23 16:55:18 -0300

    New Unet lesion segment tests.

---
+++ unet/lesionseg/data_augmentation/train_exp1_1.py
@@ -0,0 +1,301 @@
+from __future__ import print_function
+
+# GPU
+import os
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import *
+from tensorflow.keras.optimizers import Adam, SGD
+from tensorflow.keras.callbacks import ModelCheckpoint, LearningRateScheduler, History
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+from skimage.segmentation import mark_boundaries
+from skimage.exposure import rescale_intensity
+from skimage.transform import resize
+from skimage.io import imsave
+import skimage.transform as trans
+import skimage.io as io
+from sklearn.model_selection import train_test_split
+from data_covid import dice_coef, dice_coef_loss, dice_bce_loss
+
+
+BATCH_SIZE = 1
+EPOCHS = 150
+
+# model
+def unet(pretrained_weights = None,input_size = (640,640,1)):
+    # Unet original model
+    # inputs = Input(input_size)
+    # conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(inputs)
+    # conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv1)
+    # pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+    # conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool1)
+    # conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv2)
+    # pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+    # conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool2)
+    # conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv3)
+    # pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+    # conv4 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool3)
+    # conv4 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv4)
+    # drop4 = Dropout(0.5)(conv4)
+    # pool4 = MaxPooling2D(pool_size=(2, 2))(drop4)
+
+    # conv5 = Conv2D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool4)
+    # conv5 = Conv2D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv5)
+    # drop5 = Dropout(0.5)(conv5)
+
+    # up6 = Conv2D(512, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(drop5))
+    # merge6 = concatenate([drop4,up6], axis = 3)
+    # conv6 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge6)
+    # conv6 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv6)
+
+    # up7 = Conv2D(256, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv6))
+    # merge7 = concatenate([conv3,up7], axis = 3)
+    # conv7 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge7)
+    # conv7 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv7)
+
+    # up8 = Conv2D(128, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv7))
+    # merge8 = concatenate([conv2,up8], axis = 3)
+    # conv8 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge8)
+    # conv8 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv8)
+
+    # up9 = Conv2D(64, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv8))
+    # merge9 = concatenate([conv1,up9], axis = 3)
+    # conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge9)
+    # conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
+    # conv9 = Conv2D(2, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
+    # conv10 = Conv2D(1, 1, activation = 'sigmoid')(conv9)
+
+    # model = Model(inputs=[inputs], outputs=[conv10])
+    # otimizador = Adam(lr=1e-4, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.000000199)
+    
+    # 1 loss function
+    # model.compile(optimizer = otimizador,  loss=dice_coef_loss, metrics=[dice_coef], run_eagerly=True)
+
+    # 2 loss function
+    # model.compile(optimizer = otimizador,  loss=dice_bce_loss, metrics=[dice_coef], run_eagerly=True)
+
+
+
+    # Unet João Bandeira model
+    inputs = Input(input_size)
+    conv1 = Conv2D(32, (3, 3), padding='same')(inputs)
+    conv1 = LeakyReLU()(conv1)
+    conv1 = Dropout(0.2)(conv1)
+    conv1 = BatchNormalization()(conv1)
+    conv1 = Conv2D(32, (3, 3),padding='same')(conv1)
+    conv1 = LeakyReLU()(conv1)
+    conv1 = BatchNormalization()(conv1)
+    conc1 = concatenate([inputs, conv1], axis=3)
+    pool1 = AveragePooling2D(pool_size=(2, 2))(conc1)
+
+
+    conv2 = Conv2D(64, (3, 3), padding='same')(pool1)
+    conv2 = LeakyReLU()(conv2)
+    conv2 = BatchNormalization()(conv2)
+    conv2 = Dropout(0.2)(conv2)
+    conv2 = Conv2D(64, (3, 3), padding='same')(conv2)
+    conv2 = LeakyReLU()(conv2)
+    conv2 = BatchNormalization()(conv2)
+    conc2 = concatenate([pool1, conv2], axis=3)
+    pool2 = AveragePooling2D(pool_size=(2, 2))(conc2)
+
+
+    conv3 = Conv2D(128, (3, 3), padding='same')(pool2)
+    conv3 = LeakyReLU()(conv3)
+    conv3 = BatchNormalization()(conv3)
+    conv3 = Dropout(0.2)(conv3)
+    conv3 = Conv2D(128, (3, 3), padding='same')(conv3)
+    conv3 = LeakyReLU()(conv3)
+    conv3 = BatchNormalization()(conv3)
+    conc3 = concatenate([pool2, conv3], axis=3)
+    pool3 = AveragePooling2D(pool_size=(2, 2))(conc3)
+
+
+    conv4 = Conv2D(256, (3, 3), padding='same')(pool3)
+    conv4 = LeakyReLU()(conv4)
+    conv4 = BatchNormalization()(conv4)
+    conv4 = Dropout(0.2)(conv4)
+    conv4 = Conv2D(256, (3, 3), padding='same')(conv4)
+    conv4 = LeakyReLU()(conv4)
+    conv4 = BatchNormalization()(conv4)
+    conc4 = concatenate([pool3, conv4], axis=3)
+    pool4 = AveragePooling2D(pool_size=(2, 2))(conc4)
+
+
+    conv5 = Conv2D(512, (3, 3), padding='same')(pool4)
+    conv5 = LeakyReLU()(conv5)
+    conv5 = BatchNormalization()(conv5)
+    conv5 = Dropout(0.2)(conv5)
+    conv5 = Conv2D(512, (3, 3), padding='same')(conv5)
+    conv5 = LeakyReLU()(conv5)
+    conv5 = BatchNormalization()(conv5)
+    conc5 = concatenate([pool4, conv5], axis=3)
+
+
+    up6 = concatenate([Conv2DTranspose(256, (2, 2), strides=(2, 2), padding='same')(conc5), conv4], axis=3)
+    conv6 = Conv2D(256, (3, 3), padding='same')(up6)
+    conv6 = LeakyReLU()(conv6)
+    conv6 = BatchNormalization()(conv6)
+    conv6 = Dropout(0.2)(conv6)
+    conv6 = Conv2D(256, (3, 3), padding='same')(conv6)
+    conv6 = LeakyReLU()(conv6)
+    conv6 = BatchNormalization()(conv6)
+    conc6 = concatenate([up6, conv6], axis=3)
+
+
+    up7 = concatenate([Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(conc6), conv3], axis=3)
+    conv7 = Conv2D(128, (3, 3), padding='same')(up7)
+    conv7 = LeakyReLU()(conv7)
+    conv7 = BatchNormalization()(conv7)
+    conv7 = Dropout(0.2)(conv7)
+    conv7 = Conv2D(128, (3, 3), padding='same')(conv7)
+    conv7 = LeakyReLU()(conv7)
+    conv7 = BatchNormalization()(conv7)
+    conc7 = concatenate([up7, conv7], axis=3)
+
+
+    up8 = concatenate([Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(conc7), conv2], axis=3)
+    conv8 = Conv2D(64, (3, 3), padding='same')(up8)
+    conv8 = LeakyReLU()(conv8)
+    conv8 = BatchNormalization()(conv8)
+    conv8 = Dropout(0.2)(conv8)
+    conv8 = Conv2D(64, (3, 3), padding='same')(conv8)
+    conv8 = LeakyReLU()(conv8)
+    conv8 = BatchNormalization()(conv8)
+    conc8 = concatenate([up8, conv8], axis=3)
+
+
+    up9 = concatenate([Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(conc8), conv1], axis=3)
+    conv9 = Conv2D(32, (3, 3), padding='same')(up9)
+    conv9 = LeakyReLU()(conv9)
+    conv9 = Dropout(0.2)(conv9)
+    conv9 = Conv2D(32, (3, 3), padding='same')(conv9)
+    conv9 = LeakyReLU()(conv9)
+    conc9 = concatenate([up9, conv9], axis=3)
+
+
+    conv10 = Conv2D(1, (1, 1), activation='sigmoid')(conc9)
+
+    model = Model(inputs=[inputs], outputs=[conv10])
+    
+    # 1 loss function
+    model.compile(optimizer=Adam(lr=1e-5, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.000000199), loss=dice_coef_loss, metrics=['accuracy', dice_coef])
+
+    # 2 loss function
+    # model.compile(optimizer=Adam(lr=1e-5, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.000000199), loss=dice_bce_loss, metrics=['accuracy', dice_coef])
+
+    if(pretrained_weights):
+        model.load_weights(pretrained_weights)
+
+    return model
+
+def load_train_data():
+    # remote
+    imgs_train_npz = np.load('/data/flavio/anatiel/datasets/dissertacao/final_tests/train_images_exp1.npz', allow_pickle=True)
+    masks_train_npz = np.load('/data/flavio/anatiel/datasets/dissertacao/final_tests/train_masks_exp1.npz', allow_pickle=True)
+
+    imgs_train = imgs_train_npz['arr_0']
+    masks_train = masks_train_npz['arr_0']
+    
+    return np.expand_dims(np.concatenate(imgs_train), axis=-1), np.expand_dims(np.concatenate(masks_train), axis=-1)
+
+# train
+def train():
+    print('Loading and preprocessing train data...')
+    print('-'*30)
+
+    imgs_train, imgs_mask_train = load_train_data()
+    print('Loaded train images: ', imgs_train.shape, imgs_train.dtype, imgs_mask_train.shape, imgs_mask_train.dtype)
+    print('-'*30)
+
+    mean = np.mean(imgs_train)  # mean for data centering
+    std = np.std(imgs_train)  # std for data normalization
+
+    # Normalization of the train set (Exp 1)
+    imgs_train = imgs_train.astype(np.float32)
+    imgs_train -= mean
+    imgs_train /= std
+
+    print('Train test split')
+    X_train, X_test, y_train, y_test = train_test_split(imgs_train, imgs_mask_train, test_size=0.1)
+
+    print('-'*30)
+    print('Data Augmentation Start')
+    data_gen_args = dict(shear_range=0.1,
+			rotation_range=20,
+			width_shift_range=0.1, 
+			height_shift_range=0.1,
+			zoom_range=0.3,
+			fill_mode='constant',
+			horizontal_flip=True,
+			vertical_flip=True,
+			cval=0)
+    image_datagen = ImageDataGenerator(**data_gen_args)
+    mask_datagen = ImageDataGenerator(**data_gen_args)
+
+    seed = 1
+    image_datagen.fit(X_train, augment=True, seed=seed)
+    mask_datagen.fit(y_train, augment=True, seed=seed)
+
+    image_generator = image_datagen.flow(X_train, batch_size = BATCH_SIZE)
+    mask_generator = mask_datagen.flow(y_train, batch_size = BATCH_SIZE)
+
+    train = zip(image_generator, mask_generator)
+    # val = zip(X_test, y_test)
+
+    print('-'*30)
+    print('Data Augmentation End')
+    print('-'*30)
+
+    print('Creating and compiling model...')
+    print('-'*30)
+    
+    model = unet()
+    #Saving the weights and the loss of the best predictions we obtained
+    model_checkpoint = ModelCheckpoint('/data/flavio/anatiel/models/dissertacao/final_tests/unet_exp1_1_best.h5', monitor='val_loss', save_best_only=True, mode="min")
+    
+    print('Fitting model...')
+    print('-'*30)
+    history = model.fit(train,
+                        batch_size=BATCH_SIZE, 
+                        epochs=EPOCHS, 
+                        verbose=1,
+                        shuffle=True,
+                        validation_data=(X_test, y_test),
+                        # validation_split=0.1,
+                        steps_per_epoch=X_train.shape[0],
+                        callbacks=[model_checkpoint]
+                    )
+
+    model.save('/data/flavio/anatiel/models/dissertacao/final_tests/unet_exp1_1_last.h5')
+        
+    # convert the history.history dict to a pandas DataFrame:     
+    hist_df = pd.DataFrame(history.history)
+    
+    # save to json:  
+    hist_json_file = '/data/flavio/anatiel/models/dissertacao/final_tests/unet_exp1_1_hist.json'
+    with open(hist_json_file, mode='w') as f:
+        hist_df.to_json(f)
+    print("history saved")
+    
+    plt.plot(history.history['dice_coef'])
+    plt.plot(history.history['val_dice_coef'])
+    plt.title('Model dice coeff')
+    plt.ylabel('Dice coeff')
+    plt.xlabel('Epoch')
+    plt.legend(['Train', 'Val'], loc='upper left')
+    # save plot to file
+    plt.savefig('/data/flavio/anatiel/models/dissertacao/final_tests/unet_exp1_1.png')
+    # plt.show()
+    
+if __name__ == "__main__":
+    # model training
+    train()
