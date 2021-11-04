baa88e375021d82aee6f672614e7be80848bc7f8
Anatiel Santos <anatielsantos@gmail.com>
2021-11-04 19:23:32 -0300

    Setting GPU.

--- unet/lesionseg/data_augmentation/train_exp5_2.py
+++ unet/lesionseg/data_augmentation/train_exp5_2.py
@@ -3,7 +3,7 @@ from __future__ import print_function
 # GPU
 import os
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"] = "2"
+os.environ["CUDA_VISIBLE_DEVICES"] = "5"
 
 
 import numpy as np
