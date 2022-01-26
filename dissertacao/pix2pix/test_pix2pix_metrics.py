63d45d07e8ee70a5e7ae5fdbaa329d46f669eb10
anatielsantos <anatielsantos@gmail.com>
2022-01-26 09:25:00 -0300

    Ajustando paths do arquivo de calculo de metricas.

--- dissertacao/pix2pix/test_pix2pix_metrics.py
+++ dissertacao/pix2pix/test_pix2pix_metrics.py
@@ -157,7 +157,7 @@ def execExecPredictByUnet(src_dir, mask_dir, dst_dir, ext, search_pattern, model
 
     for input_path in input_pathAll:
         exam_id = os.path.basename(input_path.replace(ext, ''))
-        output_path = dst_dir + '/' + exam_id + '_PredExp2_2_last' + ext
+        output_path = dst_dir + '/' + exam_id + '_pred_k0' + ext
 
         # verifica se o arquivo ja existe
         if os.path.isfile(output_path):
@@ -192,8 +192,8 @@ def main():
     dataset = 'dataset1'
 
     # remote
-    main_dir = f'/data/flavio/anatiel/datasets/dissertacao/final_tests/kfold/dataset1/images_fold_2.npz'
-    main_mask_dir = f'/data/flavio/anatiel/datasets/dissertacao/final_tests/kfold/dataset1/masks_fold_2.npz'
+    main_dir = f'/data/flavio/anatiel/datasets/dissertacao/final_tests/kfold/{dataset}/images_fold_2.npz'
+    main_mask_dir = f'/data/flavio/anatiel/datasets/dissertacao/final_tests/kfold/{dataset}/masks_fold_2.npz'
     model_path = '/data/flavio/anatiel/models/dissertacao/unet_150epc_last.h5'
 
     src_dir = '{}'.format(main_dir)
