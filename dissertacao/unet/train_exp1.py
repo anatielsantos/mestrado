c9e40b697b72e4741158331232370295e8f3433f
anatielsantos <anatielsantos@gmail.com>
2022-01-27 20:36:43 -0300

    Removendo normalizaçao das imagens e treinando fold 0.

--- dissertacao/unet/train_exp1.py
+++ dissertacao/unet/train_exp1.py
@@ -17,7 +17,7 @@ from data_covid import load_train_data, dice_coef, dice_coef_loss, dice_bce_loss
 # GPU
 import os
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"] = "3"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 BATCH_SIZE = 1
 EPOCHS = 150
@@ -201,35 +201,35 @@ def train():
     print('Loaded train images: ', imgs_train.shape, imgs_train.dtype, imgs_mask_train.shape, imgs_mask_train.dtype)
     print('-'*30)
 
-    mean = np.mean(imgs_train)  # mean for data centering
-    std = np.std(imgs_train)  # std for data normalization
+    # mean = np.mean(imgs_train)  # mean for data centering
+    # std = np.std(imgs_train)  # std for data normalization
 
     # Normalization of the train set (Exp 1)
-    imgs_train = imgs_train.astype(np.float32)
-    imgs_train -= mean
-    imgs_train /= std
+    # imgs_train = imgs_train.astype(np.float32)
+    # imgs_train -= mean
+    # imgs_train /= std
 
     # imgs_train = imgs_train.astype(np.float32)
-    imgs_mask_train = imgs_mask_train.astype(np.float32)
+    # imgs_mask_train = imgs_mask_train.astype(np.float32)
 
     print('Creating and compiling model...')
     print('-'*30)
 
     model = unet()
     #Saving the weights and the loss of the best predictions we obtained
-    model_checkpoint = ModelCheckpoint('/data/flavio/anatiel/models/models_kfold/unet_ds2_150epc_best_k1.h5', monitor='val_loss', save_best_only=True, mode="min")
+    model_checkpoint = ModelCheckpoint('/data/flavio/anatiel/models/models_kfold/unet_ds1_150epc_best_k0.h5', monitor='val_loss', save_best_only=True, mode="min")
 
     print('Fitting model...')
     print('-'*30)
     history = model.fit(imgs_train, imgs_mask_train, batch_size=BATCH_SIZE, epochs=EPOCHS, verbose=1, shuffle=True, validation_split=0.1, callbacks=[model_checkpoint])
 
-    model.save('/data/flavio/anatiel/models/models_kfold/unet_ds2_150epc_last_k1.h5')
+    model.save('/data/flavio/anatiel/models/models_kfold/unet_ds1_150epc_last_k0.h5')
 
     # convert the history.history dict to a pandas DataFrame:     
     hist_df = pd.DataFrame(history.history)
 
     # save to json:  
-    hist_json_file = '/data/flavio/anatiel/models/models_kfold/unet_ds2_150epc_k1.json'
+    hist_json_file = '/data/flavio/anatiel/models/models_kfold/unet_ds1_150epc_k0.json'
     with open(hist_json_file, mode='w') as f:
         hist_df.to_json(f)
     print("history saved")
@@ -241,7 +241,7 @@ def train():
     plt.xlabel('Epoch')
     plt.legend(['Train', 'Val'], loc='upper left')
     # save plot to file
-    plt.savefig('/data/flavio/anatiel/models/models_kfold/unet_ds2_150epc_k1.png')
+    plt.savefig('/data/flavio/anatiel/models/models_kfold/unet_ds1_150epc_k0.png')
     # plt.show()
 
 
