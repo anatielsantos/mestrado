298f1a06112d3faab6e5c94229b762f8f403728e
anatielsantos <anatielsantos@gmail.com>
2022-01-30 11:06:24 -0300

    Alteraçao ds 2 k3.

--- dissertacao/unet/train_exp1.py
+++ dissertacao/unet/train_exp1.py
@@ -16,8 +16,8 @@ from data_covid import load_train_data, dice_coef, dice_coef_loss, dice_bce_loss
 
 BATCH_SIZE = 1
 EPOCHS = 150
-K = 2  # Definir o fold
-GPU = "0"  # Definir GPU
+K = 3  # Definir o fold
+GPU = "1"  # Definir GPU
 DS = "2"  # Definir dataset
 
 # GPU
