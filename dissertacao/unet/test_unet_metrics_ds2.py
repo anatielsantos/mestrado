49a721fc58940cdebad01f3437c01525568ecb50
marcos-dalessandro <mdclima12@gmail.com>
2022-02-04 16:53:35 -0300

    Inciando a calculos das metricas do k6

--- dissertacao/unet/test_unet_metrics_ds2.py
+++ dissertacao/unet/test_unet_metrics_ds2.py
@@ -46,8 +46,8 @@ img_rows = 544
 img_cols = 544
 img_depth = 1
 smooth = 1.
-K = "7"
-model_weights = "unet_ds2_150epc_best_k7.h5"
+K = "8"
+model_weights = "unet_ds2_150epc_best_k8.h5"
 
 
 def preprocess_squeeze(imgs):
