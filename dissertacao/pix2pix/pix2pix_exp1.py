5dc6dafef4a9d22556af8acaee2a211e74389cc7
anatielsantos <anatielsantos@gmail.com>
2022-02-10 09:25:13 -0300

    Rodando novamente k1 DS1.

--- dissertacao/pix2pix/pix2pix_exp1.py
+++ dissertacao/pix2pix/pix2pix_exp1.py
@@ -23,8 +23,8 @@ IMG_WIDTH = 544
 IMG_HEIGHT = 544
 INPUT_CHANNELS = 1
 OUTPUT_CHANNELS = 1
-K = 0     # Definir o fold
-GPU = "0"  # Definir a GPU
+K = 1  # Definir o fold
+GPU = "1"  # Definir a GPU
 DS = "1"  # Definir o dataset
 
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
