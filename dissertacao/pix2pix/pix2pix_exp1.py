8dff1206d3ce424a60ce12bf05097faa1fde6a11
anatielsantos <anatielsantos@gmail.com>
2022-02-10 08:58:53 -0300

    Corrigindo erro nos nomes dos modelos salvos.

--- dissertacao/pix2pix/pix2pix_exp1.py
+++ dissertacao/pix2pix/pix2pix_exp1.py
@@ -23,9 +23,9 @@ IMG_WIDTH = 544
 IMG_HEIGHT = 544
 INPUT_CHANNELS = 1
 OUTPUT_CHANNELS = 1
-K = 5     # Definir o fold
-GPU = "7"  # Definir a GPU
-DS = "2"  # Definir o dataset
+K = 0     # Definir o fold
+GPU = "0"  # Definir a GPU
+DS = "1"  # Definir o dataset
 
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"] = GPU
@@ -79,7 +79,7 @@ def train(path_weights, src_images_train, tar_images_train):
 
     # train model
     checkpoint = ModelCheckpoint(
-        path_weights+'gan_ds1_150epc_best_k'+str(K)+'.hdf5',
+        path_weights+'gan_ds'+DS+'_150epc_best_k'+str(K)+'.hdf5',
         monitor='dice',
         verbose=1,
         save_best_only=True,
@@ -98,14 +98,14 @@ def train(path_weights, src_images_train, tar_images_train):
         callbacks=[checkpoint]
     )
 
-    model.save(path_weights+'gan_ds1_150epc_last_k'+str(K)+'.hdf5')
+    model.save(path_weights+'gan_ds'+DS+'_150epc_last_k'+str(K)+'.hdf5')
 
     # convert the history.history dict to a pandas DataFrame:
     hist_df = pd.DataFrame(history.history)
 
     # save to json:
     print("Saving history")
-    hist_json_file = path_json+'gan_ds1_150epc_k'+str(K)+'.json'
+    hist_json_file = path_json+'gan_ds'+DS+'_150epc_k'+str(K)+'.json'
     with open(hist_json_file, mode='w') as f:
         hist_df.to_json(f)
     print("History saved")
@@ -118,7 +118,7 @@ def train(path_weights, src_images_train, tar_images_train):
     plt.xlabel('Epoch')
     plt.legend(['Train', 'Val', 'Loss'], loc='upper left')
     # save plot to file
-    plt.savefig(path_plot+'gan_ds1_150epc_k'+str(K)+'.png')
+    plt.savefig(path_plot+'gan_ds'+DS+'_150epc_k'+str(K)+'.png')
     # plt.show()
 
 
