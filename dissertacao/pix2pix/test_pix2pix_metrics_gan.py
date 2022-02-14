cb133c91c400ae95aebfb033be8d5f899d42aadf
marcos-dalessandro <mdclima12@gmail.com>
2022-02-14 19:59:31 -0300

    Retirando as metricas: DS2_Fold6

--- dissertacao/pix2pix/test_pix2pix_metrics_gan.py
+++ dissertacao/pix2pix/test_pix2pix_metrics_gan.py
@@ -190,7 +190,7 @@ def main():
     ext = '.nii.gz'
     search_pattern = '*'
     dataset = 'dataset2'
-    KF = '5'
+    KF = '6'
 
     # remote
     main_dir = f'/data/flavio/anatiel/datasets/dissertacao/final_tests/kfold/{dataset}/images/k{KF}/'
