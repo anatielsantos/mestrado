b6abf26786a5960f036df7d644ce8f222945cbc7
Pedro Tércio <pedrotercio30@gmail.com>
2022-02-15 13:28:15 -0300

    Alteracao para gan

--- dissertacao/pix2pix/test_pix2pix_metrics_unet.py
+++ dissertacao/pix2pix/test_pix2pix_metrics_unet.py
@@ -190,7 +190,7 @@ def main():
     ext = '.nii.gz'
     search_pattern = '*'
     dataset = 'dataset1'
-    KF = '4'
+    KF = '2'
 
     # remote
     main_dir = f'/data/flavio/anatiel/datasets/dissertacao/final_tests/kfold/{dataset}/images/k{KF}/'
