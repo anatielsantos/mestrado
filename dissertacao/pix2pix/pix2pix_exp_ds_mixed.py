547d6ba2b766a53e2d385b77433c480feaadb065
anatielsantos <anatielsantos@gmail.com>
2022-02-25 22:32:09 -0300

    Treino pix2pix k8.

--- dissertacao/pix2pix/pix2pix_exp_ds_mixed.py
+++ dissertacao/pix2pix/pix2pix_exp_ds_mixed.py
@@ -23,8 +23,8 @@ IMG_WIDTH = 544
 IMG_HEIGHT = 544
 INPUT_CHANNELS = 1
 OUTPUT_CHANNELS = 1
-KF = "7"  # Definir o fold
-GPU = "7"  # Definir a GPU
+KF = "8"  # Definir o fold
+GPU = "2"  # Definir a GPU
 DS = "_mixed"  # Definir o dataset
 
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
