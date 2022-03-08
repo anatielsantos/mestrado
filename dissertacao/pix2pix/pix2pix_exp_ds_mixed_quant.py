c84d7a9b4fae31717bc8ffb4b9eddfd950f2af94
anatielsantos <anatielsantos@gmail.com>
2022-03-07 22:59:28 -0300

    Trainando k2 pix2pix quant.

--- dissertacao/pix2pix/pix2pix_exp_ds_mixed_quant.py
+++ dissertacao/pix2pix/pix2pix_exp_ds_mixed_quant.py
@@ -23,8 +23,8 @@ IMG_WIDTH = 544
 IMG_HEIGHT = 544
 INPUT_CHANNELS = 1
 OUTPUT_CHANNELS = 1
-KF = "1"  # Definir o fold
-GPU = "5"  # Definir a GPU
+KF = "2"  # Definir o fold
+GPU = "6"  # Definir a GPU
 DS = "_mixed_quant"  # Definir o dataset
 
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
