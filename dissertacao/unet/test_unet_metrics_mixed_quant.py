6a11cb3ca190ac191ef1aff6597d1880ec898ad1
anatielsantos <anatielsantos@gmail.com>
2022-03-13 22:35:17 -0300

    Corrigindo erro no calculo das metricas com 8bits.

--- dissertacao/unet/test_unet_metrics_mixed_quant.py
+++ dissertacao/unet/test_unet_metrics_mixed_quant.py
@@ -91,6 +91,14 @@ def execPredict(exam_id, input_path, input_mask_path, output_path, model):
         npyMedMask = load_patient(input_mask_path)
         npyMedMask = (npyMedMask>0)*1
 
+        print(binary_masks.dtype)
+        print(np.amin(binary_masks))
+        print(np.amax(binary_masks))
+        
+        print(npyMedMask.dtype)
+        print(np.amin(npyMedMask))
+        print(np.amax(npyMedMask))
+
         # calc metrics
         print('-'*30)
         print('Calculating metrics...')
