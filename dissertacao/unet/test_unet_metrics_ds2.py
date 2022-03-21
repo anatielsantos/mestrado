b1d5e9a62b72197115ed78859b9f0c773eb767f6
anatielsantos <anatielsantos@gmail.com>
2022-03-20 23:25:22 -0300

    Refazendo arquivo de prediçao.

--- dissertacao/unet/test_unet_metrics_ds2.py
+++ dissertacao/unet/test_unet_metrics_ds2.py
@@ -179,7 +179,7 @@ def main():
     # remote   
     main_dir = f'/data/flavio/anatiel/datasets/dissertacao/final_tests/kfold/{dataset}/images/k{K}'
     main_mask_dir = f'/data/flavio/anatiel/datasets/dissertacao/final_tests/kfold/{dataset}/masks/k{K}'
-    model_path = f'/data/flavio/anatiel/models/models_kfold/unet_ds1_150epc_best_k{K}.h5'
+    model_path = f'/data/flavio/anatiel/models/models_kfold/unet_ds1_150epc_best.h5'
 
     # main_dir = f'/home/anatielsantos/mestrado/datasets/dissertacao/bbox/{dataset}/images'
     # main_mask_dir = f'/home/anatielsantos/mestrado/datasets/dissertacao/bbox/{dataset}/masks'
