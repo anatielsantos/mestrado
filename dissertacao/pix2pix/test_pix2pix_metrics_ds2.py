3d618e10729c6a7d5374d1416cd4737e0e9ce462
anatielsantos <anatielsantos@gmail.com>
2022-03-22 00:47:49 -0300

    Prediçoes novamente do experimento 3 gan.

--- dissertacao/pix2pix/test_pix2pix_metrics_ds2.py
+++ dissertacao/pix2pix/test_pix2pix_metrics_ds2.py
@@ -169,7 +169,7 @@ def execExecPredictByUnet(src_dir, mask_dir, dst_dir, ext, search_pattern, model
 
     for input_path in input_pathAll:
         exam_id = os.path.basename(input_path.replace(ext, ''))
-        output_path = dst_dir + '/' + exam_id + '_pred_exp2_gan' + ext
+        output_path = dst_dir + '/' + exam_id + '_pred_exp3_gan' + ext
 
         # verifica se o arquivo ja existe
         if os.path.isfile(output_path):
@@ -202,12 +202,12 @@ def main():
     ext = '.nii.gz'
     search_pattern = '*'
     dataset = 'dataset2'
-    KF = '2'
+    KF = '5'
 
     # remote
     main_dir = f'/data/flavio/anatiel/datasets/dissertacao/final_tests/kfold/{dataset}/images/k{KF}/'
     main_mask_dir = f'/data/flavio/anatiel/datasets/dissertacao/final_tests/kfold/{dataset}/masks/k{KF}/'
-    model_path = f'/data/flavio/anatiel/models/models_kfold/gan_ds2_150epc_best_k{KF}.hdf5'
+    model_path = f'/data/flavio/anatiel/models/models_ds1/gan_ds1_150epc_best.hdf5'
 
     # main_dir = f'/home/anatielsantos/mestrado/datasets/dissertacao/bbox/{dataset}/images'
     # main_mask_dir = f'/home/anatielsantos/mestrado/datasets/dissertacao/bbox/{dataset}/masks'
