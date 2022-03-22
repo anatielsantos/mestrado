d92c7b661405f3bd3d08636f3ca7c0c75dbc4039
anatielsantos <anatielsantos@gmail.com>
2022-03-22 00:08:37 -0300

    Prediçoes novamente do experimento 4 gan.

--- dissertacao/pix2pix/test_pix2pix_metrics_ds1.py
+++ dissertacao/pix2pix/test_pix2pix_metrics_ds1.py
@@ -195,7 +195,7 @@ def main():
     # remote
     main_dir = f'/data/flavio/anatiel/datasets/dissertacao/final_tests/kfold/{dataset}/images/k{KF}/'
     main_mask_dir = f'/data/flavio/anatiel/datasets/dissertacao/final_tests/kfold/{dataset}/masks/k{KF}/'
-    model_path = f'/data/flavio/anatiel/models/models_kfold/gan_ds2_150epc_best_k{KF}.hdf5'
+    model_path = f'/data/flavio/anatiel/models/models_ds2/gan_ds2_150epc_best.hdf5'
 
     # main_dir = f'/home/anatielsantos/mestrado/datasets/dissertacao/bbox/{dataset}/images'
     # main_mask_dir = f'/home/anatielsantos/mestrado/datasets/dissertacao/bbox/{dataset}/masks'
